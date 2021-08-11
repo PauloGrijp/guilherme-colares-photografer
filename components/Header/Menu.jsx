cabc0124ea4616a19c9f958f62d8a4a8d6649118
Paulo Xavier <pxavierf@gmail.com>
2021-08-11 16:28:48 -0300

    ending header section

---
+++ components/Header/Menu.jsx
@@ -0,0 +1,16 @@
+import { Nav } from "react-bootstrap"
+
+function Menu() {
+  return (
+    <Nav>
+      <Nav.Link href="#">Home</Nav.Link>
+      <Nav.Link href="#">About</Nav.Link>
+      <Nav.Link href="#">Galery</Nav.Link>
+      <Nav.Link href="#">Produtcs</Nav.Link>
+      <Nav.Link href="#">Experience</Nav.Link>
+      <Nav.Link href="#">Contact</Nav.Link>      
+    </Nav>
+  )
+}
+
+export default Menu
