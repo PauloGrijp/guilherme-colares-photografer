653534a832fadef4ed37dc134c424a47b42e540b
Paulo Xavier <pxavierf@gmail.com>
2021-08-11 16:31:16 -0300

    about section

---
+++ components/About/AboutImage.jsx
@@ -0,0 +1,17 @@
+import { Image } from "react-bootstrap"
+
+function AboutImage() {
+  return (
+    <div className="my-3 text-center mx-auto">
+      <Image
+        src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt="My photo"
+        className="shadow w-100"
+        style={{maxWidth:350}}
+      />
+    </div>
+  
+  )
+}
+
+export default AboutImage
