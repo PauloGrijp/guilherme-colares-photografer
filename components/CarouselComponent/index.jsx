9a4118566d489236563af44451e18f5bf455adca
Paulo Xavier <pxavierf@gmail.com>
2021-08-11 16:34:51 -0300

    carousel section

---
+++ components/CarouselComponent/index.jsx
@@ -0,0 +1,52 @@
+import { Carousel, Container, Image } from "react-bootstrap"
+
+function index() {
+  return (
+      <Carousel>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="https://s1.1zoom.me/b5050/61/New_Zealand_Scenery_Sea_443740_2560x1440.jpg"
+            alt="First Image"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="https://s1.1zoom.me/b5050/61/New_Zealand_Scenery_Sea_443740_2560x1440.jpg"
+            alt="First Image"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="https://s1.1zoom.me/b5050/61/New_Zealand_Scenery_Sea_443740_2560x1440.jpg"
+            alt="First Image"
+
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src="https://s1.1zoom.me/b5050/61/New_Zealand_Scenery_Sea_443740_2560x1440.jpg"
+            alt="First Image"
+
+          />
+        </Carousel.Item>
+      </Carousel>
+     
+    
+      // <img
+      //   className="w-screen h-screen"
+      //   src="https://s1.1zoom.me/b5050/61/New_Zealand_Scenery_Sea_443740_2560x1440.jpg"      
+      // />
+      //  <img
+      //   className="w-screen h-screen"
+      //   src="https://s1.1zoom.me/b5050/61/New_Zealand_Scenery_Sea_443740_2560x1440.jpg"      
+      // />
+
+
+  )
+}
+
+export default index
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import About from '../components/About'
+import CarouselComponent from '../components/CarouselComponent'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <CarouselComponent />
       <About />
     </div>
   )
